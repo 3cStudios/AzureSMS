1fe309e0e30bedd64a28fc88357bcb293653cd9b
Jeffery Dannel <Jeff@betterhealthinitiatives.com>
2024-09-03 19:36:18 -0400

    initial POC

---
+++ Components/Pages/Home.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using Azure;
+using Azure.Communication.Sms;
+
+
+namespace SmsPoc.Components.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        public class SmsModel
+        {
+            public string ToPhoneNumber { get; set; }
+            public string Message { get; set; }
+        }
+
+        private SmsModel ExampleSmsModel = new SmsModel();
+
+        private const string FromPhoneNumber = "YOUR AZURE COMMUNUICATION SERVICES ASSIGNED SMS PHONE NUMBER";
+        private const string ConnectionString = "YOUR AZURE COMMUNUICATION SERVICES CONNECTION STRING";
+
+
+        private void SendSms()
+        {
+            var smsClient = new SmsClient(ConnectionString);
+            try
+            {
+                smsClient.Send(
+                    from: FromPhoneNumber,
+                    to: ExampleSmsModel.ToPhoneNumber,
+                    message: ExampleSmsModel.Message
+                );
+            }
+            catch (RequestFailedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+
+    }
+}
